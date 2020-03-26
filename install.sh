122dda43fa547c223cab1b10def16a82e75a7958
Mario Mintel <mariomintel@gmail.com>
2020-03-26 20:27:55 +0100

    Update install script to create dirs if -f is set

--- install.sh
+++ install.sh
@@ -15,23 +15,27 @@ done
 git submodule init
 git submodule update
 
+# create dirs
+if ! [ -z ${force} ]; then
+	mkdir -p ~/.cache
+	mkdir -p ~/.vim
+	mkdir -p ~/.config/nvim
+	mkdir -p ~/.config/rofi
+	mkdir -p ~/.config/oomox/colors/
+fi
+
 # standard
 ln -sv$force $PWD/zshrc ~/.zshrc
 ln -sv$force $PWD/vimrc ~/.vimrc
 ln -sv$force $PWD/coc-settings.json ~/.vim/coc-settings.json
-ln -sv$force $PWD/alacritty.yml ~/.config/alacritty.yml
 ln -sv$force $PWD/tmux.conf ~/.tmux.conf
 ln -sv$force $PWD/screenrc ~/.screenrc
 ln -sv$force $PWD/gitconfig ~/.gitconfig
+
+ln -sv$force $PWD/awesome ~/.config/
+ln -sv$force $PWD/alacritty.yml ~/.config/alacritty.yml
 ln -sv$force $PWD/compton.conf ~/.config/compton.conf
 ln -sv$force $PWD/themes/rofi/* ~/.config/rofi/
 ln -sv$force $PWD/themes/oomox/* ~/.config/oomox/colors/
-
-# nvim
-if [[ -d ~/.config/nvim ]]; then
-        ln -sv$force $PWD/init.vim ~/.config/nvim/init.vim
-        ln -sv$force $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
-fi
-
-# awesome
-ln -sv$force $PWD/awesome ~/.config/
+ln -sv$force $PWD/init.vim ~/.config/nvim/init.vim
+ln -sv$force $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
