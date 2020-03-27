5b8b8ccb5db5a1d5d37f32896a996fb4ea089ee7
Mario Mintel <mariomintel@gmail.com>
2020-03-27 10:39:26 +0100

    awesome: add customized layout icons

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -4,6 +4,7 @@ local dpi = xresources.apply_dpi
 
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
+local layout_path = gfs.get_configuration_dir() .. "/icons/layouts/"
 local icon_path = gfs.get_configuration_dir() .. "/icons/tags/"
 
 local theme = {}
@@ -62,22 +63,10 @@ theme.titlebar_close_button_normal = icon_path .. "close.png"
 theme.titlebar_close_button_focus  = icon_path .. "close.png"
 
 -- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.layout_fairv = layout_path .."fairv.png"
+theme.layout_floating = layout_path .."floating.png"
+theme.layout_tilebottom = layout_path .."tilebottom.png"
+theme.layout_tile = layout_path .."tile.png"
 
 theme.wallpaper = "/usr/share/backgrounds/psy_forest.png"
 
