0b5a5c9735afa9a1f5107df242a056543be8b323
Mario Mintel <mariomintel@gmail.com>
2020-07-31 12:16:15 +0200

    install.sh: fix bug

--- install.sh
+++ install.sh
@@ -20,19 +20,22 @@ if ! [ -z ${force} ]; then
 	mkdir -p ~/.config/oomox/colors/
 fi
 
-# standard
-ln -sv$force $PWD/zshrc ~/.zshrc
+# vim stuff
 ln -sv$force $PWD/vim/vimrc ~/.vimrc
+ln -sv$force $PWD/vim/init.vim ~/.config/nvim/init.vim
 ln -sv$force $PWD/vim/autoload/* ~/.vim/autoload/
 ln -sv$force $PWD/vim/coc-settings.json ~/.vim/coc-settings.json
+ln -sv$force $PWD/vim/coc-settings.json ~/.config/nvim/coc-settings.json
+
+# cli tools
+ln -sv$force $PWD/zshrc ~/.zshrc
 ln -sv$force $PWD/tmux.conf ~/.tmux.conf
 ln -sv$force $PWD/screenrc ~/.screenrc
 ln -sv$force $PWD/gitconfig ~/.gitconfig
 ln -sv$force $PWD/tigrc ~/.tigrc
 
+# others
 ln -sv$force $PWD/alacritty.yml ~/.config/alacritty.yml
 ln -sv$force $PWD/picom.conf ~/.config/picom.conf
 ln -sv$force $PWD/themes/rofi/* ~/.config/rofi/
 ln -sv$force $PWD/themes/oomox/* ~/.config/oomox/colors/
-ln -sv$force $PWD/init.vim ~/.config/nvim/init.vim
-ln -sv$force $PWD/coc-settings.json ~/.config/nvim/coc-settings.json
