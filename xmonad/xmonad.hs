9a8ad8d34c2ba35225d861002dfa6653455b2dec
Mario Mintel <mariomintel@gmail.com>
2021-02-19 19:52:08 +0100

    xmonad: add gaps

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,14 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Layout.IndependentScreens
+import XMonad.Layout.Spacing
 import qualified XMonad.StackSet as W
 
 myTerminal    = "alacritty"
+myRunMenu     = "rofi -show run -theme gruvbox.rasi -lines 7"
 myModMask     = mod4Mask -- Win key
 myBorderWidth = 2
+myGapWidth    = 10
 myWorkspaces  = ["1", "2", "3", "4"]
 
 main = xmonad $ def {
@@ -13,13 +16,14 @@ main = xmonad $ def {
     , modMask     = myModMask
     , borderWidth = myBorderWidth
     , workspaces  = withScreens 2 myWorkspaces
+    , layoutHook  = myLayoutHook
     }
     `additionalKeysP` myKeybinds
 
 myKeybinds =
     -- applications
     [ ("M-<Return>", spawn myTerminal)
-    , ("M-<Space>", spawn "rofi -show run -theme gruvbox.rasi -lines 7")
+    , ("M-<Space>", spawn myRunMenu)
     ]
     ++
     -- layout
@@ -32,3 +36,6 @@ myKeybinds =
     -- movement
     [ ("M-w", kill)
     ]
+
+myLayoutHook =
+    spacingRaw False (Border 0 0 0 0) False(Border myGapWidth myGapWidth myGapWidth myGapWidth) True $ layoutHook def
