e672f77da786efee358470c7cb21c3099a746914
Mario Mintel <mariomintel@gmail.com>
2021-07-21 19:57:37 +0200

    nvim: add severity filter for lsp signs

--- nvim/lua/plugins/config/lspconfig.lua
+++ nvim/lua/plugins/config/lspconfig.lua
@@ -88,7 +88,42 @@ function M.setup_servers()
     end
 end
 
+function M.setup_severity_filter()
+    local orig_set_signs = vim.lsp.diagnostic.set_signs
+    local set_signs_limited = function(diagnostics, bufnr, client_id, sign_ns, opts)
+
+    -- early escape
+    if not diagnostics then
+        return
+    end
+
+    -- Work out max severity diagnostic per line
+    local max_severity_per_line = { }
+    for _,d in pairs(diagnostics) do
+        if max_severity_per_line[d.range.start.line] then
+        local current_d = max_severity_per_line[d.range.start.line]
+        if d.severity < current_d.severity then
+            max_severity_per_line[d.range.start.line] = d
+        end
+        else
+        max_severity_per_line[d.range.start.line] = d
+        end
+    end
+
+    -- map to list
+    local filtered_diagnostics = { }
+    for _,v in pairs(max_severity_per_line) do
+        table.insert(filtered_diagnostics, v)
+    end
+
+    -- call original function
+    orig_set_signs(filtered_diagnostics, bufnr, client_id, sign_ns, opts)
+    end
+    vim.lsp.diagnostic.set_signs = set_signs_limited
+end
+
 return function()
+    M.setup_severity_filter()
     M.setup_servers()
     M.setup_icons()
 end
