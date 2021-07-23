1c6e166d5fc59b4b337086bad948b2a7649d489f
Mario Mintel <mariomintel@gmail.com>
2021-07-23 16:43:08 +0200

    nvim: add neogit and scrollview

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -18,69 +18,69 @@ local function conf(name)
 end
 
 return require('packer').startup(function()
-    -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- Colorscheme
     use {
         'npxbr/gruvbox.nvim',
-        requires = { 'rktjmp/lush.nvim' },
+        requires = 'rktjmp/lush.nvim',
         config = conf('gruvbox'),
     }
 
-    -- Fuzzy finder
     use {
         'nvim-telescope/telescope.nvim',
-        requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
+        requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
         config = conf('telescope')
     }
 
-    -- LSP
     use {
         'neovim/nvim-lspconfig',
         config = conf('lspconfig'),
     }
 
-    -- Completion
     use {
         'nvim-lua/completion-nvim',
         config = conf('completion'),
     }
 
-    -- Treesitter
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
         config = conf('treesitter'),
     }
 
-    -- Bufferline
     use {
         'akinsho/nvim-bufferline.lua',
         requires = 'kyazdani42/nvim-web-devicons',
         config = conf('bufferline'),
     }
 
-    --[[ Statusline
-    use {
-        'datwaft/bubbly.nvim',
-    }
-    --]]
-
-    -- Lightspeed
-    use 'ggandor/lightspeed.nvim'
-
-    -- git hunks/signs
     use {
         'lewis6991/gitsigns.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim'
-        },
+        requires = 'nvim-lua/plenary.nvim',
         config = conf('gitsigns'),
+    }
+
+    use {
+        'TimUntersberger/neogit',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('neogit').setup({ })
+        end
     }
 
     use {
         'lukas-reineke/indent-blankline.nvim',
         config = conf('indentline'),
     }
+
+    use {
+        'dstein64/nvim-scrollview',
+        config = function()
+            require('telescope').setup({
+                scrollview_blend = 100,
+            })
+        end,
+    }
+
+    use 'ggandor/lightspeed.nvim'
 end)
