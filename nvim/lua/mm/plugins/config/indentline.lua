0b41c50d5b97345565998ed83fb8a8cf2328b121
Mario Mintel <mariomintel@gmail.com>
2021-07-28 12:44:04 +0200

    nvim: change indentline config

--- nvim/lua/mm/plugins/config/indentline.lua
+++ nvim/lua/mm/plugins/config/indentline.lua
@@ -1,7 +1,6 @@
 return function()
-    vim.g.indent_blankline_char = '│' --
-    vim.g.indent_blankline_use_treesitter = true
-    vim.g.indent_blankline_show_first_indent_level = false
+    vim.g.indent_blankline_char = '│' --┊ ┆ 
+    -- vim.g.indent_blankline_show_first_indent_level = false
     vim.g.indent_blankline_show_trailing_blankline_indent = false
     vim.g.indent_blankline_show_current_context = true
     vim.g.indent_blankline_context_highlight_list = { 'Delimiter' }
