ae0c9a888f99c9e770573751a119be74cd0d56b3
Mario Mintel <mariomintel@gmail.com>
2021-09-15 09:09:57 +0200

    remove deprecated mappings from bufferline

--- nvim/lua/mm/plugins/config/bufferline.lua
+++ nvim/lua/mm/plugins/config/bufferline.lua
@@ -1,7 +1,6 @@
 return function()
     require('bufferline').setup({
         options = {
-            mappings = false,
             right_mouse_command = '',
             middle_mouse_command = 'bdelete! %d',
             show_close_icon = false,
