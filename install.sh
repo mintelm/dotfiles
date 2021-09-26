c0afdbd6b8b7a52a23b0f737ff7e71ad3f7ddb31
Mario Mintel <mariomintel@gmail.com>
2021-09-26 15:24:09 +0200

    delete some configs, update install script

--- install.sh
+++ install.sh
@@ -11,37 +11,28 @@ while getopts "f" opt; do
         esac
 done
 
-set -x
 # create dirs
 if ! [ -z ${force} ]; then
+        echo Creating requires directories...
         mkdir -p ~/.cache
         mkdir -p ~/.zsh
-        mkdir -p ~/.xmonad
         mkdir -p ~/.vim/autoload
-        mkdir -p ~/.config/nvim
-        mkdir -p ~/.config/rofi
         mkdir -p ~/.config/bat
         mkdir -p ~/.config/kitty
-        mkdir -p ~/.config/oomox/colors/
 fi
 
 # zsh stuff
+echo Linking zsh stuff...
 ln -sv$force $PWD/zsh/zshrc ~/.zshrc
 ln -sv$force $PWD/zsh/manydots-magic ~/.zsh
 ln -sv$force $PWD/zsh/*.*sh ~/.zsh/
 
-# xmonad
-ln -sv$force $PWD/xmonad/* ~/.xmonad/
-
-# vim stuff
-# ln -sv$force $PWD/vim/vimrc ~/.vimrc
-# ln -sv$force $PWD/vim/init.vim ~/.config/nvim/init.vim
-# ln -sv$force $PWD/vim/autoload/* ~/.vim/autoload/
-# ln -sv$force $PWD/vim/coc-settings.json ~/.vim/coc-settings.json
-# ln -sv$force $PWD/vim/coc-settings.json ~/.config/nvim/coc-settings.json
+# nvim stuff
+echo Linking nvim stuff...
 ln -sv$force $PWD/nvim/ ~/.config/
 
 # cli tools
+echo Linking cli stuff...
 ln -sv$force $PWD/cli/tmux.conf ~/.config/tmux.conf
 ln -sv$force $PWD/cli/screenrc ~/.config/screenrc
 ln -sv$force $PWD/cli/gitconfig ~/.gitconfig
@@ -49,12 +40,8 @@ ln -sv$force $PWD/cli/tigrc ~/.config/tigrc
 ln -sv$force $PWD/cli/batconfig ~/.config/bat/config
 
 # utils
+echo Linking utils stuff...
 ln -sv$force $PWD/utils/kitty.conf ~/.config/kitty/kitty.conf
 ln -sv$force $PWD/utils/alacritty.yml ~/.config/alacritty.yml
 ln -sv$force $PWD/utils/dunstrc ~/.config/dunstrc
-ln -sv$force $PWD/utils/picom.conf ~/.config/picom.conf
 ln -sv$force $PWD/utils/Xresources ~/.Xresources
-
-# themes
-ln -sv$force $PWD/themes/rofi/* ~/.config/rofi/
-ln -sv$force $PWD/themes/oomox/* ~/.config/oomox/colors/
