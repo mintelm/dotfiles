596a3ce20b52fafaa3a2a0123c03eadaddb48df4
Mario Mintel <mariomintel@gmail.com>
2021-09-26 15:43:04 +0200

    update config

--- nvim/lua/mm/plugins/config/indentline.lua
+++ nvim/lua/mm/plugins/config/indentline.lua
@@ -1,5 +1,5 @@
 return function()
-    vim.g.indent_blankline_char = '┊' --┆ │ 
+    vim.g.indent_blankline_char = '│' --┆ ┊ 
     vim.g.indent_blankline_show_first_indent_level = false
     vim.g.indent_blankline_show_trailing_blankline_indent = false
     vim.g.indent_blankline_show_current_context = false
