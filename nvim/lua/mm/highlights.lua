5dc296288a475b5b479ccd7cbbdbf1222cad6aa6
Mario Mintel <mariomintel@gmail.com>
2021-11-17 20:21:37 +0100

    update highlights.lua with new comments

--- nvim/lua/mm/highlights.lua
+++ nvim/lua/mm/highlights.lua
@@ -1,5 +1,3 @@
--- Resources
--- 1. https://github.com/akinsho/dotfiles/blob/main/.config/nvim/lua/as/highlights.lua
 local fmt = string.format
 
 local M = { }
@@ -14,37 +12,41 @@ local function hex_to_rgb(color)
   return tonumber(hex:sub(1, 2), 16), tonumber(hex:sub(3, 4), 16), tonumber(hex:sub(5), 16)
 end
 
-local function alter(attr, percent)
-  return math.floor(attr * (100 + percent) / 100)
-end
-
+---Darken a specified hex color
 ---@source https://stackoverflow.com/q/5560248
 ---@see: https://stackoverflow.com/a/37797380
----Darken a specified hex color
 ---@param color string
 ---@param percent number
 ---@return string
 function M.darken_color(color, percent)
+    local alter = function(attr, percent)
+        return math.floor(attr * (100 + percent) / 100)
+    end
     local r, g, b = hex_to_rgb(color)
+
     if not r or not g or not b then
         return 'NONE'
     end
+
     r, g, b = alter(r, percent), alter(g, percent), alter(b, percent)
     r, g, b = math.min(r, 255), math.min(g, 255), math.min(b, 255)
+
     return string.format('#%02x%02x%02x', r, g, b)
 end
 
---- Check if the current window has a winhighlight
---- which includes the specific target highlight
---- @param win_id integer
---- @vararg string
+---Check if the current window has a winhighlight
+---which includes the specific target highlight
+---@param win_id integer
+---@vararg string
 function M.has_win_highlight(win_id, ...)
     local win_hl = vim.wo[win_id].winhighlight
+
     for _, target in ipairs { ... } do
         if win_hl:match(target) ~= nil then
         return true, win_hl
         end
     end
+
     return false, win_hl
 end
 
@@ -58,11 +60,13 @@ function M.adopt_winhighlight(win_id, target, name, default)
     name = name .. win_id
     local _, win_hl = M.has_win_highlight(win_id, target)
     local hl_exists = vim.fn.hlexists(name) > 0
+
     if not hl_exists then
         local parts = vim.split(win_hl, ',')
         local found = mm.find(parts, function(part)
             return part:match(target)
         end)
+
         if found then
             local hl_group = vim.split(found, ':')[2]
             local bg = M.get_hl(hl_group, 'bg')
@@ -71,12 +75,13 @@ function M.adopt_winhighlight(win_id, target, name, default)
             M.set_hl(name, { guibg = bg, guifg = fg, gui = gui })
         end
     end
+
     return name
 end
 
-
---- NOTE: vim.highlight's link and create are private, so
---- eventually move to using `nvim_set_hl`
+---Set highlight group
+---NOTE: vim.highlight's link and create are private, so
+---eventually move to using `nvim_set_hl`
 ---@param name string
 ---@param opts table
 function M.set_hl(name, opts)
@@ -92,7 +97,6 @@ function M.set_hl(name, opts)
         end
     end
 end
-
 
 ---convert a table of gui values into a string
 ---@param hl table<string, string>
@@ -134,10 +138,13 @@ function M.get_hl(grp, attr, fallback)
     return '#' .. bit.tohex(color, 6)
 end
 
+---Clear a highlight group
+---@param name string
 function M.clear_hl(name)
     if not name then
         return
     end
+
     vim.cmd(fmt('highlight clear %s', name))
 end
 
