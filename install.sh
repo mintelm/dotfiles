e9a67751d3041f331a6ae345cc17d0a0d0f6a581
Mario Mintel <mariomintel@gmail.com>
2021-11-18 13:11:54 +0100

    update install script so it can get called from outside directories

--- install.sh
+++ install.sh
@@ -1,48 +1,40 @@
 #!/bin/bash
 
-# GETOPTS
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # reset in case getopts has been used previously in shell
 OPTIND=1
-force=''
 while getopts "f" opt; do
         case "$opt" in
-                f) force='f'
-                   ;;
+                f)
+                        force='f'
+                        echo Creating required directories...
+                        mkdir -p ~/.cache ~/.zsh ~/.config/nvim ~/.config/bat ~/.config/kitty
+                        ;;
         esac
 done
 
-# create dirs
-if ! [ -z ${force} ]; then
-        echo Creating requires directories...
-        mkdir -p ~/.cache
-        mkdir -p ~/.zsh
-        mkdir -p ~/.vim/autoload
-        mkdir -p ~/.config/nvim
-        mkdir -p ~/.config/bat
-        mkdir -p ~/.config/kitty
-fi
-
 # zsh stuff
 echo Linking zsh stuff...
-ln -sv$force $PWD/zsh/zshrc ~/.zshrc
-ln -sv$force $PWD/zsh/manydots-magic ~/.zsh
-ln -sv$force $PWD/zsh/*.*sh ~/.zsh
+ln -sv$force $SCRIPT_DIR/zsh/zshrc ~/.zshrc
+ln -sv$force $SCRIPT_DIR/zsh/manydots-magic ~/.zsh
+ln -sv$force $SCRIPT_DIR/zsh/*.*sh ~/.zsh
 
 # nvim stuff
 echo Linking nvim stuff...
-ln -sv$force $PWD/nvim/* ~/.config/nvim
+ln -sv$force $SCRIPT_DIR/nvim/* ~/.config/nvim
 
 # cli tools
 echo Linking cli stuff...
-ln -sv$force $PWD/cli/tmux.conf ~/.config/tmux.conf
-ln -sv$force $PWD/cli/screenrc ~/.config/screenrc
-ln -sv$force $PWD/cli/gitconfig ~/.gitconfig
-ln -sv$force $PWD/cli/tigrc ~/.config/tigrc
-ln -sv$force $PWD/cli/batconfig ~/.config/bat/config
-ln -sv$force $PWD/cli/Xresources ~/.Xresources
+ln -sv$force $SCRIPT_DIR/cli/tmux.conf ~/.config/tmux.conf
+ln -sv$force $SCRIPT_DIR/cli/screenrc ~/.config/screenrc
+ln -sv$force $SCRIPT_DIR/cli/gitconfig ~/.gitconfig
+ln -sv$force $SCRIPT_DIR/cli/tigrc ~/.config/tigrc
+ln -sv$force $SCRIPT_DIR/cli/batconfig ~/.config/bat/config
+ln -sv$force $SCRIPT_DIR/cli/Xresources ~/.Xresources
 
 # utils
 echo Linking utils stuff...
-ln -sv$force $PWD/utils/kitty.conf ~/.config/kitty/kitty.conf
-ln -sv$force $PWD/utils/alacritty.yml ~/.config/alacritty.yml
-ln -sv$force $PWD/utils/dunstrc ~/.config/dunstrc
+ln -sv$force $SCRIPT_DIR/utils/kitty.conf ~/.config/kitty/kitty.conf
+ln -sv$force $SCRIPT_DIR/utils/alacritty.yml ~/.config/alacritty.yml
+ln -sv$force $SCRIPT_DIR/utils/dunstrc ~/.config/dunstrc
