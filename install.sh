7e1d1097c1af74af90f5d619fd6e9dc9a0093940
Mario Mintel <mariomintel@gmail.com>
2021-11-18 13:16:17 +0100

    rename variable in install script

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ OPTIND=1
 while getopts "f" opt; do
         case "$opt" in
                 f)
-                        force='f'
+                        f='f'
                         echo Creating required directories...
                         mkdir -p ~/.cache ~/.zsh ~/.config/nvim ~/.config/bat ~/.config/kitty
                         ;;
@@ -23,25 +23,25 @@ done
 
 # zsh stuff
 echo Linking zsh stuff...
-ln -sv$force $SCRIPT_DIR/zsh/zshrc ~/.zshrc
-ln -sv$force $SCRIPT_DIR/zsh/manydots-magic ~/.zsh
-ln -sv$force $SCRIPT_DIR/zsh/*.*sh ~/.zsh
+ln -sv$f $SCRIPT_DIR/zsh/zshrc ~/.zshrc
+ln -sv$f $SCRIPT_DIR/zsh/manydots-magic ~/.zsh
+ln -sv$f $SCRIPT_DIR/zsh/*.*sh ~/.zsh
 
 # nvim stuff
 echo Linking nvim stuff...
-ln -sv$force $SCRIPT_DIR/nvim/* ~/.config/nvim
+ln -sv$f $SCRIPT_DIR/nvim/* ~/.config/nvim
 
 # cli tools
 echo Linking cli stuff...
-ln -sv$force $SCRIPT_DIR/cli/tmux.conf ~/.config/tmux.conf
-ln -sv$force $SCRIPT_DIR/cli/screenrc ~/.config/screenrc
-ln -sv$force $SCRIPT_DIR/cli/gitconfig ~/.gitconfig
-ln -sv$force $SCRIPT_DIR/cli/tigrc ~/.config/tigrc
-ln -sv$force $SCRIPT_DIR/cli/batconfig ~/.config/bat/config
-ln -sv$force $SCRIPT_DIR/cli/Xresources ~/.Xresources
+ln -sv$f $SCRIPT_DIR/cli/tmux.conf ~/.config/tmux.conf
+ln -sv$f $SCRIPT_DIR/cli/screenrc ~/.config/screenrc
+ln -sv$f $SCRIPT_DIR/cli/gitconfig ~/.gitconfig
+ln -sv$f $SCRIPT_DIR/cli/tigrc ~/.config/tigrc
+ln -sv$f $SCRIPT_DIR/cli/batconfig ~/.config/bat/config
+ln -sv$f $SCRIPT_DIR/cli/Xresources ~/.Xresources
 
 # utils
 echo Linking utils stuff...
-ln -sv$force $SCRIPT_DIR/utils/kitty.conf ~/.config/kitty/kitty.conf
-ln -sv$force $SCRIPT_DIR/utils/alacritty.yml ~/.config/alacritty.yml
-ln -sv$force $SCRIPT_DIR/utils/dunstrc ~/.config/dunstrc
+ln -sv$f $SCRIPT_DIR/utils/kitty.conf ~/.config/kitty/kitty.conf
+ln -sv$f $SCRIPT_DIR/utils/alacritty.yml ~/.config/alacritty.yml
+ln -sv$f $SCRIPT_DIR/utils/dunstrc ~/.config/dunstrc
