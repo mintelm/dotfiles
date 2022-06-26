687b9552d7bb7aacce0378a9e296a39023352d86
Mario Mintel <mariomintel@gmail.com>
2022-06-26 18:02:37 +0200

    nvim: fix errors in keymappings

--- nvim/lua/mm/keymappings.lua
+++ nvim/lua/mm/keymappings.lua
@@ -5,22 +5,22 @@ local silenced = { silent = true }
 vim.g.mapleader = ','
 
 -- unmap
-mm.map('', '<C-b>', '')
-mm.map('', '<C-f>', '')
+mm.map('', '<C-b>', '', {})
+mm.map('', '<C-f>', '', {})
 
 -- navigation
-mm.map('', ']q', ':cnext<CR>')
-mm.map('', '[q', ':cprev<CR>')
-mm.map('', ']Q', ':clast<CR>')
-mm.map('', '[Q', ':cfirst<CR>')
+mm.map('', ']q', ':cnext<CR>', {})
+mm.map('', '[q', ':cprev<CR>', {})
+mm.map('', ']Q', ':clast<CR>', {})
+mm.map('', '[Q', ':cfirst<CR>', {})
 
 -- clear highlight on space
 mm.map('n', '<Space>', '<cmd>noh<CR>', silenced)
 
 -- telescope
-mm.map('n', '<leader>f', '<cmd>Telescope find_files<CR>')
-mm.map('n', '<leader>gf', '<cmd>Telescope git_files<CR>')
-mm.map('n', '<leader>gr', '<cmd>Telescope live_grep<CR>')
+mm.map('n', '<leader>f', '<cmd>Telescope find_files<CR>', {})
+mm.map('n', '<leader>gf', '<cmd>Telescope git_files<CR>', {})
+mm.map('n', '<leader>gr', '<cmd>Telescope live_grep<CR>', {})
 
 -- bufferline
 mm.map('n', ']b', '<cmd>BufferLineCycleNext<CR>', silenced)
@@ -38,16 +38,16 @@ mm.map('n', '<leader>8', '<cmd>BufferLineGoToBuffer 8<CR>', silenced)
 mm.map('n', '<leader>9', '<cmd>BufferLineGoToBuffer 9<CR>', silenced)
 
 -- gitsigns
-mm.map('n', '<leader>hv', '<cmd>lua require"gitsigns".preview_hunk()<CR>')
-mm.map('n', '<leader>ha', '<cmd>lua require"gitsigns".stage_hunk()<CR>')
-mm.map('n', '<leader>hu', '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>')
-mm.map('n', '<leader>hr', '<cmd>lua require"gitsigns".reset_hunk()<CR>')
-mm.map('n', '<leader>gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-mm.map('n', ']h', '<cmd>lua require"gitsigns".next_hunk()<CR>')
-mm.map('n', '[h', '<cmd>lua require"gitsigns".prev_hunk()<CR>')
+mm.map('n', '<leader>hv', '<cmd>lua require"gitsigns".preview_hunk()<CR>', {})
+mm.map('n', '<leader>ha', '<cmd>lua require"gitsigns".stage_hunk()<CR>', {})
+mm.map('n', '<leader>hu', '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>', {})
+mm.map('n', '<leader>hr', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {})
+mm.map('n', '<leader>gb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>', {})
+mm.map('n', ']h', '<cmd>lua require"gitsigns".next_hunk()<CR>', {})
+mm.map('n', '[h', '<cmd>lua require"gitsigns".prev_hunk()<CR>', {})
 
 -- neogit
-mm.map('n', '<leader>gs', '<cmd>lua require"neogit".open()<CR>')
+mm.map('n', '<leader>gs', '<cmd>lua require"neogit".open()<CR>', {})
 
 -- lsp
 -- this function is passed to lsp's on_attach hook, so mappings are only loaded if lsp is
