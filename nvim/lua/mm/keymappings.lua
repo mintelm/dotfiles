5311e8b05e367693ec4c76a9c910221e84549436
Mario Mintel <mariomintel@gmail.com>
2022-06-28 15:08:04 +0200

    nvim: add keymappings for hop

--- nvim/lua/mm/keymappings.lua
+++ nvim/lua/mm/keymappings.lua
@@ -1,6 +1,9 @@
 local M = { }
 
 local silenced = { silent = true }
+local cmd = function(command)
+    return table.concat({ '<cmd>', command, '<CR>' })
+end
 
 vim.g.mapleader = ','
 
@@ -15,22 +18,31 @@ mm.map('', ']Q', ':clast<CR>', {})
 mm.map('', '[Q', ':cfirst<CR>', {})
 
 -- clear highlight on space
-mm.map('n', '<Space>', '<cmd>noh<CR>', silenced)
+mm.map('n', '<Space>', cmd 'noh', silenced)
 
 -- bufferline
-mm.map('n', ']b', '<cmd>BufferLineCycleNext<CR>', silenced)
-mm.map('n', '[b', '<cmd>BufferLineCyclePrev<CR>', silenced)
-mm.map('n', '<leader>bf', '<cmd>BufferLinePick<CR>', silenced)
-mm.map('n', '<leader>bd', '<cmd>BufferLinePickClose<CR>', silenced)
-mm.map('n', '<leader>1', '<cmd>BufferLineGoToBuffer 1<CR>', silenced)
-mm.map('n', '<leader>2', '<cmd>BufferLineGoToBuffer 2<CR>', silenced)
-mm.map('n', '<leader>3', '<cmd>BufferLineGoToBuffer 3<CR>', silenced)
-mm.map('n', '<leader>4', '<cmd>BufferLineGoToBuffer 4<CR>', silenced)
-mm.map('n', '<leader>5', '<cmd>BufferLineGoToBuffer 5<CR>', silenced)
-mm.map('n', '<leader>6', '<cmd>BufferLineGoToBuffer 6<CR>', silenced)
-mm.map('n', '<leader>7', '<cmd>BufferLineGoToBuffer 7<CR>', silenced)
-mm.map('n', '<leader>8', '<cmd>BufferLineGoToBuffer 8<CR>', silenced)
-mm.map('n', '<leader>9', '<cmd>BufferLineGoToBuffer 9<CR>', silenced)
+mm.map('n', ']b', cmd 'BufferLineCycleNext', silenced)
+mm.map('n', '[b', cmd 'BufferLineCyclePrev', silenced)
+mm.map('n', '<leader>bf', cmd 'BufferLinePick', silenced)
+mm.map('n', '<leader>bd', cmd 'BufferLinePickClose', silenced)
+mm.map('n', '<leader>1', cmd 'BufferLineGoToBuffer 1', silenced)
+mm.map('n', '<leader>2', cmd 'BufferLineGoToBuffer 2', silenced)
+mm.map('n', '<leader>3', cmd 'BufferLineGoToBuffer 3', silenced)
+mm.map('n', '<leader>4', cmd 'BufferLineGoToBuffer 4', silenced)
+mm.map('n', '<leader>5', cmd 'BufferLineGoToBuffer 5', silenced)
+mm.map('n', '<leader>6', cmd 'BufferLineGoToBuffer 6', silenced)
+mm.map('n', '<leader>7', cmd 'BufferLineGoToBuffer 7', silenced)
+mm.map('n', '<leader>8', cmd 'BufferLineGoToBuffer 8', silenced)
+mm.map('n', '<leader>9', cmd 'BufferLineGoToBuffer 9', silenced)
+
+-- hop
+mm.map('n', 's', cmd 'HopChar2AC', silenced)
+mm.map('n', 'S', cmd 'HopChar2BC', silenced)
+-- not dot repeatable yet ...
+-- mm.map('', 'f', cmd "lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })", {})
+-- mm.map('', 'F', cmd "lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })", {})
+-- mm.map('', 't', cmd "lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })", {})
+-- mm.map('', 'T', cmd "lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })", {})
 
 -- lsp
 -- this function is passed to lsp's on_attach hook, so mappings are only loaded if lsp is
