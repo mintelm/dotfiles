6c5764f61dd4ae2b7d997e567dde0f0404922e0e
Mario Mintel <mariomintel@gmail.com>
2022-06-30 10:32:00 +0200

    nvim: rice telescope

--- nvim/lua/mm/plugins/telescope.lua
+++ nvim/lua/mm/plugins/telescope.lua
@@ -1,8 +1,26 @@
 return function()
     local actions = require('telescope.actions')
+    local function reorder_border()
+        local border = { }
+        local order = { 2, 4, 6, 8, 1, 3, 5, 7 }
+        for _, i in pairs(order) do
+            table.insert(border, mm.style.current.border[i])
+        end
+
+        return border
+    end
+
+    require('mm.highlights').plugin('telescope', {
+        TelescopeNormal = { link = 'NormalFloat' },
+        TelescopeBorder = { link = 'FloatBorder' },
+    })
+
     require('telescope').setup({
         defaults = {
             prompt_prefix = ' ',
+            border = { },
+            borderchars = reorder_border(),
+            set_env = { ["COLORTERM"] = "truecolor" },
             mappings = {
                 i = {
                     ['<c-c>'] = function()
