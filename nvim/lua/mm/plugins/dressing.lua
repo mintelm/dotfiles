bc5e6abb8397ebd346cb70b2c1e9c7bbae63fc12
Mario Mintel <mariomintel@gmail.com>
2022-06-30 21:33:12 +0200

    nvim: update dressing config

--- nvim/lua/mm/plugins/dressing.lua
+++ nvim/lua/mm/plugins/dressing.lua
@@ -1,12 +1,14 @@
 return function()
-    --require('mm.highlights').plugin('dressing', { FloatTitle = { inherit = 'Visual', bold = true } })
+    local bc = mm.style.current.border
+
     require('dressing').setup({
         input = {
             border = mm.style.current.border,
         },
         select = {
             telescope = require('telescope.themes').get_cursor({
-                --borderchars = mm.style.current.border,
+                -- telescope expects different order
+                borderchars = { bc[2], bc[4], bc[6], bc[8], bc[1], bc[3], bc[5], bc[7] },
             }),
         },
     })
