ca272ae7fbb6c86264d4f166343fd13fa8c4f371
Mario Mintel <mariomintel@gmail.com>
2022-11-17 17:03:08 +0100

    nvim: install all treesitter parsers

--- nvim/lua/mm/plugins/treesitter.lua
+++ nvim/lua/mm/plugins/treesitter.lua
@@ -1,8 +1,6 @@
 return function ()
     require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-            'c', 'lua', 'latex', 'bibtex', 'python', 'rust',
-        },
+        ensure_installed = 'all',
         highlight = {
             enable = true,
         },
