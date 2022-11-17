0de154c9e7bece48582317a42e8b7520223a9e76
Mario Mintel <mariomintel@gmail.com>
2022-11-17 22:31:06 +0100

    nvim: use telescope finders for lsp stuff

--- nvim/lua/mm/plugins/init.lua
+++ nvim/lua/mm/plugins/init.lua
@@ -22,7 +22,7 @@ require('packer').startup({function(use)
         'nvim-telescope/telescope.nvim',
         requires = {
             { 'nvim-lua/plenary.nvim' },
-            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+            { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
         },
         config = conf('telescope'),
     }
