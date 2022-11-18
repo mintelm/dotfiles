1cfe0a88b59462718defc6a79f577ad526e65ccb
Mario Mintel <mariomintel@gmail.com>
2022-11-18 18:12:32 +0100

    nvim: change git sign colors of theme

--- nvim/lua/mm/plugins/github-nvim-theme.lua
+++ nvim/lua/mm/plugins/github-nvim-theme.lua
@@ -1,6 +1,19 @@
 return function()
+    local theme_style = 'dark_default'
+    local colors = require('github-theme.palette').get_palette(theme_style)
+
     require('github-theme').setup({
-        theme_style = 'dark_default',
+        theme_style = theme_style,
         function_style = 'bold',
+        comment_style = 'NONE',
+        keyword_style = 'NONE',
+        variable_style = 'NONE',
+        colors = {
+            git_signs = {
+                add = colors.git.add,
+                change = colors.git.change,
+                delete = colors.git.delete,
+            },
+        },
     })
 end
