3a8973b25d689040c00a394dd17c3d78434f4f5a
Mario Mintel <mario.mintel@vector.com>
2023-02-11 13:04:52 +0100

    update notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,6 +1,10 @@
 return {
     'rcarriga/nvim-notify',
     config = function()
+        require('notify').setup({
+            max_width = 100,
+            minimum_width = 25,
+        })
         vim.notify = require('notify')
     end,
 }
