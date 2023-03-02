bc58c3783cc48880dedf78b5b17aa9c3fd26384e
Mario Mintel <mariomintel@gmail.com>
2023-03-02 23:13:49 +0100

    add dependency to leap

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -4,6 +4,7 @@ return {
         config = function()
             require('leap').add_default_mappings()
         end,
+        dependencies = 'tpope/vim-repeat',
     },
     {
         'ggandor/flit.nvim',
