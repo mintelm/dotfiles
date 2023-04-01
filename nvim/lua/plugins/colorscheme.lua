1631e42e7096b95343587cc47573cf80bd44dbca
Mario Mintel <mariomintel@gmail.com>
2023-04-01 16:11:29 +0200

    change color

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -22,7 +22,7 @@ local function config()
     vim.api.nvim_set_hl(0, 'BufferLineTabSelected', { link = 'BufferLineBufferSelected' })
 
     -- vscode like style
-    vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#282C34', fg = 'NONE' })
+    vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#161b22', fg = 'NONE' })
     vim.api.nvim_set_hl(0, 'Pmenu', { fg = '#C5CDD9', bg = '#22252A' })
 
     vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { fg = '#7E8294', bg = 'NONE', strikethrough = true })
