e2c8c171aa4c77aa9c8ca951ed5726b58d1d578d
Mintel, Mario <mario.mintel@vector.com>
2023-09-13 12:38:02 +0200

    add LineLimit usercmd

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -52,3 +52,17 @@ vim.api.nvim_create_user_command(
     end,
     { nargs = 0 }
 )
+
+vim.api.nvim_create_user_command(
+    'LineLimit',
+    function(opts)
+        local line_limit = opts.args + 0
+        local hl_fg = vim.api.nvim_get_hl_by_name('Normal', true).foreground
+        local hl_bg = vim.api.nvim_get_hl_by_name('Visual', true).background
+        vim.api.nvim_set_hl(0, 'LineLimit', { fg = hl_fg, bg = hl_bg })
+
+        if (line_limit > 0) then line_limit = line_limit + 1 end
+        vim.cmd('match LineLimit \'\\%' .. line_limit .. 'v.\\+\'')
+    end,
+    { nargs = 1 }
+)
