9c540cb1cd72e1a861e22a3b62d8332868d164bb
Mintel, Mario <mario.mintel@vector.com>
2024-04-03 13:58:34 +0200

    update bufferline config

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,24 +1,25 @@
-local function config()
-    require('bufferline').setup({
-        highlights = require('catppuccin.groups.integrations.bufferline').get(),
-        options = {
-            numbers = 'ordinal',
-            close_command = 'bwipeout %d',
-            right_mouse_command = '',
-            middle_mouse_command = 'bwipeout %d',
-            separator_style = 'thin',
-            persist_buffer_sort = false,
-            show_buffer_close_icons = false,
-            modified_icon = require('style').icons.ui.modified,
-        },
-    })
-end
-
 return {
     'akinsho/bufferline.nvim',
-    config = config,
+    event = 'VeryLazy',
+    after = 'catppuccin',
     dependencies = {
-        { 'kyazdani42/nvim-web-devicons' },
-        { 'catppuccin' },
+        'kyazdani42/nvim-web-devicons',
+        'catppuccin',
     },
+    config = function()
+        require('bufferline').setup({
+            -- catppuccin integration needs to be set via config entry
+            highlights = require('catppuccin.groups.integrations.bufferline').get(),
+            options = {
+                numbers = 'ordinal',
+                close_command = 'bwipeout %d',
+                right_mouse_command = '',
+                middle_mouse_command = 'bwipeout %d',
+                separator_style = 'thin',
+                persist_buffer_sort = false,
+                show_buffer_close_icons = false,
+                modified_icon = require('style').icons.ui.modified,
+            },
+        })
+    end,
 }
