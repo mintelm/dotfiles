efac1ac298759e5bdb54dcd3350d7f1dd0efd3b1
Mintel, Mario <mario.mintel@vector.com>
2024-04-03 13:48:52 +0200

    remove incline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -102,34 +102,5 @@ return {
             { 'kyazdani42/nvim-web-devicons' },
             { 'catppuccin' },
         },
-    },
-    {
-        'b0o/incline.nvim',
-        config = function()
-            local devicons = require('nvim-web-devicons')
-            require('incline').setup {
-                window = {
-                    padding = 0,
-                    margin = { horizontal = 0 },
-                },
-                render = function(props)
-                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
-                    if filename == '' then
-                        filename = '[No Name]'
-                    end
-                    local ft_icon, ft_color = devicons.get_icon_color(filename)
-                    local modified = vim.bo[props.buf].modified
-                    return {
-                        ft_icon and { ' ', ft_icon, ' ', guifg = ft_color } or '',
-                        ' ',
-                        { filename, gui = modified and 'bold,italic' or 'bold' },
-                        ' ',
-                        guibg = '#2a2b3c',
-                    }
-                end,
-            }
-        end,
-        dependencies = { 'kyazdani42/nvim-web-devicons' },
-        event = 'VeryLazy',
-    },
+    }
 }
