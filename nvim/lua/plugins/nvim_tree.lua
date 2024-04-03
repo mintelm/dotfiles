35fbd10c824caad4fde46bcb37cca01cd5ee239a
Mintel, Mario <mario.mintel@vector.com>
2024-04-03 14:21:14 +0200

    update nvim tree config

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -1,10 +1,11 @@
 return {
     'nvim-tree/nvim-tree.lua',
-    config = function()
+    event = 'VeryLazy',
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    init = function()
         -- disable netrw at the very start of your init.lua (strongly advised)
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-        require('nvim-tree').setup()
     end,
-    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = true,
 }
