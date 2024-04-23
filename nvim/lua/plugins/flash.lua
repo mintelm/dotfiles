4594b58928ad45482b62854210277d23b90584f4
Mintel, Mario <mario.mintel@vector.com>
2024-04-23 17:13:26 +0200

    update flash configuration

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,19 +1,18 @@
 return {
-    {
-        'folke/flash.nvim',
-        event = 'VeryLazy',
-        opts = {
-            jump = { autojump = true }, -- auto jump if there is only one label
-            modes = {
-                search = { enabled = false },
-                char = {
-                    jump_labels = true,
-                    multi_line = false,
-                },
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+        jump = { autojump = true }, -- auto jump if there is only one label
+        modes = {
+            search = { enabled = false },
+            char = {
+                jump_labels = true,
+                multi_line = false,
+                jump = { autojump = true },
             },
         },
-        keys = {
-            { 's', mode = { 'n', 'o', 'x' }, function() require('flash').jump() end, desc = 'Flash' },
-        },
+    },
+    keys = {
+        { 's', mode = { 'n', 'o', 'x' }, function() require('flash').jump() end, desc = 'Flash' },
     },
 }
