0541e630fe813bdc1386ccebf4caf12f20ccbd53
Mintel, Mario <mario.mintel@vector.com>
2024-04-23 17:29:32 +0200

    fix treesitter configuration

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,16 +2,18 @@ return {
     'nvim-treesitter/nvim-treesitter',
     event = { 'BufReadPre', 'BufNewFile', 'VeryLazy' },
     dependencies = 'HiPhish/rainbow-delimiters.nvim',
-    opts = {
-        auto_install = true,
-        ensure_installed = {
-            'gitcommit',
-            'diff',
-            'git_rebase',
-        },
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        },
-    },
+    config = function()
+        require('nvim-treesitter.configs').setup({
+            auto_install = true,
+            ensure_installed = {
+                'gitcommit',
+                'diff',
+                'git_rebase',
+            },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
 }
