4ffde3a95e22c95ad9d9d0b2f0090951b5de93c0
Mintel, Mario <mario.mintel@vector.com>
2024-05-29 14:02:43 +0200

    remove severity filter

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -29,29 +29,6 @@ local function diagnostics_config()
 
         vim.fn.sign_define(sign, { text = icon, texthl = hl })
     end
-
-    -- setup severity filter
-    local ns = vim.api.nvim_create_namespace('diagonstic_severity_filter')
-    local orig_signs_handler = vim.diagnostic.handlers.signs
-    vim.diagnostic.handlers.signs = {
-        show = function(_, bufnr, _, opts)
-            local diagnostics = vim.diagnostic.get(bufnr)
-
-            local max_severity_per_line = {}
-            for _, d in pairs(diagnostics) do
-                local m = max_severity_per_line[d.lnum]
-                if not m or d.severity < m.severity then
-                    max_severity_per_line[d.lnum] = d
-                end
-            end
-
-            local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
-            orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
-        end,
-        hide = function(_, bufnr)
-            orig_signs_handler.hide(ns, bufnr)
-        end,
-    }
 end
 
 local function lsp_config()
