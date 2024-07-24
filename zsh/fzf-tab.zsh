145e8e0757de3b724f5bbcda8e963e432a7864ed
Mintel, Mario <mario.mintel@vector.com>
2024-07-24 11:31:52 +0200

    update fzf tab config

--- zsh/fzf-tab.zsh
+++ zsh/fzf-tab.zsh
@@ -4,6 +4,8 @@ zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:*:descriptions' format '[%d]'
 # set list-colors to enable filename colorizing
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
+zstyle ':completion:*' menu no
 # preview directory's content with exa when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
 # switch group using `,` and `.`
