4109a5923d1d42ac69c0155f4ebf3cf364ee8f53
Mintel, Mario <mario.mintel@vector.com>
2024-08-02 09:47:39 +0200

    update nvim-tree config

--- nvim/lua/plugins/nvim_tree.lua
+++ nvim/lua/plugins/nvim_tree.lua
@@ -2,5 +2,11 @@ return {
     'nvim-tree/nvim-tree.lua',
     event = 'VeryLazy',
     dependencies = 'kyazdani42/nvim-web-devicons',
-    config = true,
+    opts = {
+        actions = {
+            open_file = {
+                resize_window = false,
+            },
+        },
+    },
 }
