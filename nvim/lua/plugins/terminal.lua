cf7de9b2077a1cb3b250321ba7e4617386ca2bb5
Mintel, Mario <mario.mintel@vector.com>
2024-08-02 09:47:50 +0200

    fix toggleterm issue on :qa with autocmd

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,3 +1,23 @@
+local utils = require('utils')
+
+-- fix exit behavior of toggle term (E948: Job still running) on :qa
+utils.augroup('ToggleTermFix', {
+    {
+        event = { 'TermEnter' },
+        command = function()
+            for _, buffers in ipairs(vim.fn.getbufinfo()) do
+                local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, 'filetype')
+                if filetype == 'toggleterm' then
+                    vim.api.nvim_create_autocmd({ 'BufWriteCmd', 'FileWriteCmd', 'FileAppendCmd' }, {
+                        buffer = buffers.bufnr,
+                        command = 'q!',
+                    })
+                end
+            end
+        end,
+    },
+})
+
 return {
     'akinsho/toggleterm.nvim',
     event = 'VeryLazy',
