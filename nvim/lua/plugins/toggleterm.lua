8357f3a14e42cd0930f4ba55aac9b289a0edc630
Mario Mintel <mario.mintel@vector.com>
2025-07-22 13:16:47 +0200

    Update toggleterm settings

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,32 +1,11 @@
 local cmd = require('utils').cmd
-local augroup = require('utils').augroup
 
 return {
     'akinsho/toggleterm.nvim',
     event = 'VeryLazy',
-    config = function()
-        require('toggleterm').setup({
-            start_in_insert = false,
-        })
-
-        -- fix exit behavior of toggle term (E948: Job still running) on :qa
-        augroup('ToggleTermFix', {
-            {
-                event = { 'TermEnter' },
-                command = function()
-                    for _, buffers in ipairs(vim.fn.getbufinfo()) do
-                        local filetype = vim.api.nvim_buf_get_option(buffers.bufnr, 'filetype')
-                        if filetype == 'toggleterm' then
-                            vim.api.nvim_create_autocmd({ 'BufWriteCmd', 'FileWriteCmd', 'FileAppendCmd' }, {
-                                buffer = buffers.bufnr,
-                                command = 'q!',
-                            })
-                        end
-                    end
-                end,
-            },
-        })
-    end,
+    opts = {
+        start_in_insert = false,
+    },
     keys = {
         { '<leader>t', cmd('ToggleTerm direction=float') },
         { '<esc>',     '<C-\\><C-n>',                    mode = 't' },
