52ea22c7830804b0ebde98c7e468b34ec6a13e03
Mario Mintel <mario.mintel@vector.com>
2025-11-11 12:09:46 +0100

    Update zsh keybinds

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -3,8 +3,10 @@ bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey '^R' history-incremental-pattern-search-backward
-bindkey "^[[3~" delete-char
-bindkey '^[[Z' reverse-menu-complete
-bindkey "^[[1;5C" forward-word
-bindkey "^[[1;5D" backward-word
+bindkey '^R' history-incremental-pattern-search-backward # ctrl + r
+bindkey -M isearch '^J' history-incremental-search-forward # ctrl + j
+bindkey -M isearch '^K' history-incremental-search-backward # ctrl + k
+bindkey "^[[3~" delete-char # del
+bindkey '^[[Z' reverse-menu-complete # shift + tab
+bindkey "^[[1;5C" forward-word # ctrl + <-
+bindkey "^[[1;5D" backward-word # ctrl + ->
