2b4043296b7b67cd2fa3df77f02ef3a790c37ed0
Mario Mintel <mario.mintel@vector.com>
2026-01-27 10:49:30 +0100

    Fix lessfilter for ubuntu and arch

--- zsh/lessfilter.sh
+++ zsh/lessfilter.sh
@@ -4,16 +4,18 @@
 
 has_cmd() { command -v "$1" &> /dev/null || { echo "$1 is not installed"; return 1; }; }
 
+# Ubuntu uses 'batcat' instead of 'bat'
+BAT=$(command -v bat || command -v batcat)
 MIME=$(file -bL --mime-type "$1")
 CATEGORY=${MIME%%/*}
-BAT_CMD="bat --color=always -p"
+BAT_CMD="$BAT --color=always -p"
 
 if [[ -d "$1" ]]; then
     has_cmd eza && eza -1 --color=always --git --icons --group-directories-first "$1"
 elif [[ "$CATEGORY" == "image" ]]; then
     has_cmd chafa && chafa "$1"
 elif [[ "$CATEGORY" == "text" ]]; then
-    has_cmd bat && $BAT_CMD "$1"
+    [[ -n "$BAT" ]] && $BAT_CMD "$1"
 else
-    has_cmd lesspipe.sh && has_cmd bat && lesspipe.sh "$1" | $BAT_CMD
+    has_cmd lesspipe.sh && [[ -n "$BAT" ]] && lesspipe.sh "$1" | $BAT_CMD
 fi
