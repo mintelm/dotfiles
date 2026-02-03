e136f26aa20d00b01999a04c3100a3f0b82ed53b
Mario Mintel <mario.mintel@vector.com>
2026-02-03 11:15:27 +0100

    Update treesitter

--- nvim/lua/mivim/plugins/treesitter.lua
+++ nvim/lua/mivim/plugins/treesitter.lua
@@ -1,28 +1,15 @@
+local languages = { 'c', 'cpp' }
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = languages,
+    callback = function() vim.treesitter.start() end,
+})
+
 return {
     'nvim-treesitter/nvim-treesitter',
-    event = 'VeryLazy',
-    dependencies = {
-        'HiPhish/rainbow-delimiters.nvim',
-        'nvim-treesitter/nvim-treesitter-refactor',
-    },
-    config = function()
-        require('nvim-treesitter.configs').setup({
-            auto_install = true,
-            ensure_installed = {
-                'gitcommit',
-                'diff',
-                'git_rebase',
-            },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            refactor = {
-                highlight_definitions = {
-                    enable = true,
-                    clear_on_cursor_move = false,
-                },
-            },
-        })
+    lazy = false, -- plugin does not support lazy-loading.
+    build = ':TSUpdate',
+    init = function()
+        require('nvim-treesitter').install(languages)
     end,
 }
