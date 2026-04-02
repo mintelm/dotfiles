bcc7dd855107ade71a70dcd5843b35600d2be489
Mintel, Mario <mario.mintel@vector.com>
2026-04-02 14:34:06 +0200

    Update code-companion config

--- nvim/lua/mivim/plugins/copilot.lua
+++ nvim/lua/mivim/plugins/copilot.lua
@@ -1,28 +1,21 @@
 return {
-    {
-        'olimorris/codecompanion.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        opts = {
-            adapters = {
-                http = {
-                    copilot = function()
-                        return require('codecompanion.adapters').extend('copilot', {
-                            schema = {
-                                model = {
-                                    default = 'claude-opus-4.6',
-                                },
-                            },
-                        })
-                    end,
-                }
+    'olimorris/codecompanion.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+        interactions = {
+            chat = {
+                adapter = {
+                    name = 'copilot_acp',
+                    model = 'claude-opus-4.6',
+                },
             },
         },
-        keys = {
-            { '<leader>cc', function() require('codecompanion').toggle() end, desc = 'Toggle Code Companion Chat', },
-        },
+    },
+    keys = {
+        { '<leader>cc', function() require('codecompanion').toggle() end, desc = 'Toggle Code Companion Chat', },
     },
 }
